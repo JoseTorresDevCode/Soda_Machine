b4d712ef3349fb04a2863c2c2c344438adc149c8
JoseTorresDevCode <SgtJoseTorres@yahoo.com>
2020-08-20 00:07:59 -0500

    added list and method to wallet for coins

--- SodaMachine/SodaMachine.cs
+++ SodaMachine/SodaMachine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,12 +20,49 @@ namespace SodaMachine
         {
             machineInventory = new List<Can>();
             register = new List<Coin>();
-
+            FillRegister();
+            FillSodaMachine();
         }
             
         //Member Methods
+        public void FillRegister()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                register.Add(new Quarter());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                register.Add(new Dime());
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                register.Add(new Nickel());
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                register.Add(new Penny());
+            }
 
+            
+        }
 
-        //make selections
+        public void FillSodaMachine()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                machineInventory.Add(new OrangeSoda());
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                machineInventory.Add(new RootBeer());
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                machineInventory.Add(new Cola());
+            }
+        }
+
+        
     }
 }
--- SodaMachine/Wallet.cs
+++ SodaMachine/Wallet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,37 @@ namespace SodaMachine
     class Wallet
     {
         //Member Variables
-        
+        public List<Coin> coins;
+
         //Constructor
+        public Wallet()
+        {
+            coins = new List<Coin>;
+            AddWalletCoins();
+        }
+
 
         //Member Methods
                  //payment method
-                 
+        
+        public void AddWalletCoins()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                coins.Add(new Quarter());
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                coins.Add(new Dime());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                coins.Add(new Nickel());
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                coins.Add(new Penny());
+            }
+        }         
     }
 }
